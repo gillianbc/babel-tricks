da1ba2c6a3cf06e6dc87b7553d160ce712c75657
gillianbc <gszemeti@gmail.com>
2019-07-24 15:30:35 +0100

    Cleaned up

--- babel-tricks-rc/lib/index.js
+++
@@ -1,8 +0,0 @@
-const aprom = word => {
-    return Promise.resolve().then(function () {
-        return console.log('I promise to say ' + word);
-    }).then(function () {
-        console.log('Done');
-    });
-};
-//# sourceMappingURL=index.js.map
